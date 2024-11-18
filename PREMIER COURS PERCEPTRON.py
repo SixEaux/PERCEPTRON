72546483f509485116a8b45bf98165711d909579
SixEaux <dc200421@gmail.com>
2024-11-18 22:48:08 +0100

    Y

--- PREMIER COURS PERCEPTRON.py
+++ PREMIER COURS PERCEPTRON.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
 import pprint
+import random
 
 
 with open('valeursentraine', 'rb') as f:
@@ -25,14 +26,15 @@ class ImageReader():
 
 
 class Perceptron:
-    def __init__(self,nbneurones, *, coefcv = 0.1, iterations=1000, seuil = 0):
+    def __init__(self, nbneurones, pix, vales, *, coefcv = 0.1, iterations=1000, seuil = 0):
         self.iter = iterations
         self.nb = nbneurones
-        self.poids = [1 for _ in range(nbneurones+1)]
+        self.poids = [1 for _ in range(nbneurones)]
         self.cvcoef = coefcv
         self.seuil = seuil
         self.biais = 1
-
+        self.pix = pix
+        self.vales = vales
 
     def fctactiv(self, x):
         if x>self.seuil:
@@ -45,10 +47,9 @@ class Perceptron:
             fx = 0
             for i in range(len(tab1)):
                 fx += tab1[i]*tab2[i]
-            print(fx)
             return fx
         except TypeError:
-            print(tab1,"\n", tab2)
+            print("oh non erreur", tab1,"\n", tab2)
             return
 
     def changerpoids(self, attendu, observation, input):
@@ -57,7 +58,6 @@ class Perceptron:
         for i in range(1, len(self.poids)):
             self.poids[i] = self.cvcoef * (attendu - observation) * vrainput[i]
         self.poids[0] += self.cvcoef * (attendu - observation)
-
 
 
     def vraivaleur(self, recherchee, valeur):
@@ -72,22 +72,25 @@ class Perceptron:
         activ = self.fctactiv(attendu)
         return 1 if activ == 1 else 0
 
-    def entrainementuns(self, pix, val):#erreur de penser
-        for fig in range(len(pix)):
-            pred = self.prediction(pix[fig])
-            self.changerpoids(self.vraivaleur(1, val[fig]), pred, pix[fig])
+    def entrainementuns(self):#erreur de penser
+        for fig in range(len(self.pix)):
+            pred = self.prediction(self.pix[fig])
+            self.changerpoids(self.vraivaleur(1, self.vales[fig]), pred, self.pix[fig])
+
+    def tauxerreur(self):
+        correct = 0
+        for i in range(len(self.pix)):
+            predator = self.prediction(self.pix[i])
+            if predator == self.vraivaleur(1, self.vales[i]):
+                correct += 1
+        return 100 - correct*100/len(self.pix)
 
 
-P = Perceptron(784)
-P.entrainementuns(pixels,valeurs)
+P = Perceptron(784, pixels, valeurs)
 
+print(P.poids)
+P.entrainementuns()
+print(P.poids)
 
-
-def cherche1(tab):
-    for i in range(len(tab)):
-        if tab[i] == 1:
-            return i
-a = cherche1(valeurs)
-print(P.prediction(pixels[a]))
-print(P.vraivaleur(1, valeurs[a]))
+print(P.tauxerreur())
 
