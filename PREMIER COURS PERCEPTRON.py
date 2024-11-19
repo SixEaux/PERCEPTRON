908f5ec0d1f7895197ee2237caf75a88f5bb2e82
SixEaux <dc200421@gmail.com>
2024-11-19 08:51:34 +0100

    Y

--- PREMIER COURS PERCEPTRON.py
+++ PREMIER COURS PERCEPTRON.py
@@ -29,7 +29,7 @@ class Perceptron:
     def __init__(self, nbneurones, pix, vales, *, coefcv = 0.1, iterations=1000, seuil = 0):
         self.iter = iterations
         self.nb = nbneurones
-        self.poids = [random.randint(0, 100000) for _ in range(nbneurones)]
+        self.poids = [random.randint(0, 10000000) for _ in range(nbneurones)]
         self.cvcoef = coefcv
         self.seuil = seuil
         self.biais = 1
@@ -109,8 +109,8 @@ class Perceptron:
 
 P = Perceptron(784, pixels, valeurs)
 
-P.entrainementun(8)
+P.entrainementun(2)
 P.autreautreprint(P.poids)
 
-print(P.tauxerreur(8))
+print(P.tauxerreur(2))
 
