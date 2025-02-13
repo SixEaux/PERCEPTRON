64f5a0a4ef1e203f9ad2859d15ddf41b19acc36a
SixEaux <dc200421@gmail.com>
2025-02-13 21:46:10 +0100

    Y

--- LAYER.py
+++ LAYER.py
@@ -1,6 +1,45 @@
 import numpy as np
-from Functions import SoftMax
+
+class Layer:
+    def __init__(self, nbneurons, acti, lnginputs):
+
+        self.weights = np.random.uniform(-1, 1, (lnginputs, nbneurons))
+        self.bias = np.random.uniform(-1, 1, (nbneurons, 1))
+
+        if acti == 'sigmoid':
+            self.activation =  lambda x: 1 / (1 + np.exp(-x))
+            self.difactivation = lambda x: np.exp(-x) / (1 + np.square(np.exp(-x)))
+
+        elif acti == 'relu':
+            self.activation =  lambda x: np.where(x > 0, x, 0)
+            self.difactivation = lambda x: np.where(x > 0, x, 0)
+
+        elif acti == 'tanh':
+            self.activation =  lambda x: np.tanh(x)
+            self.difactivation = lambda x: 1 - np.square(np.tanh(x))
+
+        else:
+            pass
+
+    def forward(self, input):
+        return self.activation(np.dot(np.transpose(self.weights), input) + self.bias)
+
+    def backward(self, diffnextlayer):
+        pass
 
 
+class OutputLayer(Layer):
+    def __init__(self, nbneurons, activation, lnginputs):
+        super().__init__(nbneurons, activation, lnginputs)
 
+    def softmax(self, input):
+        return np.exp(input) / np.sum(np.exp(input), axis=0)
 
+    def softmaxdiff(self, output):
+        pass
+
+    def forward(self, input):
+        return self.softmax(np.dot(np.transpose(self.weights), input) + self.bias)
+
+    def backward(self, diffnextlayer):
+        pass
--- Réseau multicouche.py
+++ Réseau multicouche.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
 from tabulate import tabulate
+from LAYER import Layer, OutputLayer
 
 
 # Plus tard:
@@ -29,47 +30,6 @@ def takeinputs():
 
     return valeurs, pixels
 
-
-class Layer:
-    def __init__(self, nbneurons, acti, lnginputs):
-
-        self.weights = np.ones((lnginputs, nbneurons))
-        self.bias = np.ones((nbneurons, 1))
-
-        if acti == 'sigmoid':
-            self.activation =  lambda x: 1 / (1 + np.exp(-x))
-            self.difactivation = lambda x: np.exp(-x) / (1 + np.square(np.exp(-x)))
-
-        elif acti == 'relu':
-            self.activation =  lambda x: np.where(x > 0, x, 0)
-            self.difactivation = lambda x: np.where(x > 0, x, 0)
-
-        elif acti == 'tanh':
-            self.activation =  lambda x: np.tanh(x)
-            self.difactivation = lambda x: 1 - np.square(np.tanh(x))
-
-    def forward(self, input):
-        return self.activation(np.dot(np.transpose(self.weights), input) + self.bias)
-
-    def backward(self, diffnextlayer):
-        pass
-
-
-class OutputLayer(Layer):
-    def __init__(self, nbneurons, activation, lnginputs):
-        super().__init__(nbneurons, activation, lnginputs)
-
-    def softmax(self, input):
-        return np.exp(input) / np.sum(np.exp(input), axis=1, keepdims=True)
-
-    def softmaxdiff(self, output):
-        pass
-
-    def forward(self, input):
-        return self.softmax(np.dot(np.transpose(self.weights), input) + self.bias)
-
-    def backward(self, diffnextlayer):
-        pass
 
 class NN:
     def __init__(self, pix, vales, nblayer, infolay, errorfunc, *, coefcv=0.1, iterations=10):
@@ -116,7 +76,6 @@ class NN:
     def forwardprop(self, input): #forward all the layers until output
         for i in range(len(self.layers)):
             res = self.layers[i].forward(input)
-            print(res)
             input = res
         return input
 
@@ -132,11 +91,11 @@ class NN:
 
 
 val, pix = takeinputs()
-lay = [[64, "relu", 784]]
+lay = [[64, "sigmoid", 784]]
 
 g = NN(pix, val, 1, lay, "eqm")
 
 
-g.printbasesimple(pix[10].reshape(784,1))
+g.printbasesimple(pix[10].reshape(784,1)/255)
 l = g.forwardprop((pix[10].reshape(784,1))/255)
-print(l.shape)
+print(l)
